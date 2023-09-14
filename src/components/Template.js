81f4673bef2f2b325f00d3964e3e84bc4de0a2b1
prince-arun <prince.skillsafari@gmail.com>
2023-09-15 03:01:17 +0530

    navigation bug fixed

--- src/components/Template.js
+++ src/components/Template.js
@@ -1,12 +1,7 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
 import { AnimeContext } from "../context/context";
-import animeL from "../assets/logo.png";
-import Container from "react-bootstrap/Container";
-import Navbar from "react-bootstrap/Navbar";
-import NewEdit from "./NewEdit";
-import Button from "react-bootstrap/Button";
 
 const Template = () => {
   const { getAnimePictures, pictures } = AnimeContext();
@@ -20,32 +15,20 @@ const Template = () => {
 
   useEffect(() => {
     getAnimePictures(id);
-  }, [getAnimePictures, id]);
+  }, [id]);
+
+  const navigate = useNavigate();
+  const goBack = () => {
+    navigate(-1);
+  };
   return (
     <div className="gallery">
       {/* ---------------------Navbar----------------- */}
-      <Navbar className="bg-dark">
-        <Container>
-          <Navbar.Brand href="#home">
-            <img src={animeL} alt="anime-logo" width={180} />
-          </Navbar.Brand>
-          <Navbar.Toggle />
-          <Navbar.Collapse className="justify-content-end">
-            <Link to={"/signin/:id"}>
-              <Button variant="outline-secondary" className="ps-10px">
-                Sign Out
-              </Button>{" "}
-            </Link>
-            <NewEdit id={id} />
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+
       {/* ----------------------------------------------- */}
-      <div className="back">
-        <Link to="/home">
-          <i className="fas fa-arrow-left"></i>
-          Back to Home
-        </Link>
+      <div className="back" onClick={goBack}>
+        <i className="fas fa-arrow-left"></i>
+        Back to Home
       </div>
       <div className="big-image">
         <img src={pictures[index]?.jpg.image_url} alt="" />
